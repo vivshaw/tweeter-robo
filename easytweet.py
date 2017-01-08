6d09ba4b4200965b9b2cf77de86b4d366c63a08d
vivshaw <hvivianshaw@gmail.com>
2017-01-07 21:33:56 -0500

    write skeleton of TweetBot class and main method

--- easytweet.py
+++ easytweet.py
@@ -5,12 +5,37 @@ Created on Sat Jan  7 19:27:34 2017
 @author: vivshaw
 """
 
+import sys
 import tweepy
+from time import sleep
 from secrets import *
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret);
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+class TweetBot:
+    def __init__(self, corpus, delay):
+        #initialize Twitter authorization with Tweepy
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_key, access_secret)
+        self.api = tweepy.API(self.auth)
+        
+        #initialize other fields
+        self.corpus = TweetBot.loadCorpus(corpus)
+        self.delay = delay
+    
+    @staticmethod
+    def loadCorpus(corpus):
+        return corpus
+        
+    def tweet(self, message):
+        self.api.update_status(message)
 
-#tweet = 'Hello, world! Just testing Python twitter automation with tweepy'
-#api.update_status(status=tweet)
+    def automate(self):
+        pass
+    
+def main(args):
+    args = args[1:]
+    bot = TweetBot(args[0], args[1])
+    print(bot.corpus, bot.delay)
+    bot.tweet("Hello World!")
+
+if __name__ == "__main__":
+    main(sys.argv)
