c117cbb3a8a130c4d583534da78f3b1ba71f4c34
vivshaw <hvivianshaw@gmail.com>
2017-01-13 14:58:12 -0500

    Add convenience wrapper

---
+++ tweeter_robo_go.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running tweeter_robo directly from source tree."""
+ 
+from tweeter_robo.tweeter_robo import main
+
+if __name__ == '__main__':
+    main()
