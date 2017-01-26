4fcf201eb9135b277aba1f3f04aaf3b83ad557e8
vivshaw <hvivianshaw@gmail.com>
2017-01-26 02:36:41 -0500

    General tidying

--- tweeter_robo/tweeter_robo.py
+++ tweeter_robo/tweeter_robo.py
@@ -19,6 +19,7 @@ if os.getenv("HEROKU"):
 else:
     from tweeter_robo.twitter_credentials import consumer_key, consumer_secret, access_token, access_token_secret
 
+
 class TweetBot:
     def __init__(self, corpus):
         self.load_corpus(corpus)
@@ -52,12 +53,6 @@ class TweetBot:
     
     def reply(self, reply_within):
         pass
-
-
-def load_corpus(corpus):
-    with open(corpus) as corpus_file:
-        corpus_lines = corpus_file.read()
-    return corpus_lines
 
 
 def main():
