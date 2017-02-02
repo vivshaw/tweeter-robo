92dc39b10476b189ef385aa0d9f003a8bfa1a0cb
vivshaw <hvivianshaw@gmail.com>
2017-02-02 04:58:34 -0500

    Scratch reply

--- tweeter_robo/tweeter_robo.py
+++ tweeter_robo/tweeter_robo.py
@@ -24,7 +24,7 @@ class TweetBot:
     def __init__(self, corpus):
         self.load_corpus(corpus)
 
-        #initialize Twitter authorization with Tweepy
+        # initialize Twitter authorization with Tweepy
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
@@ -33,7 +33,7 @@ class TweetBot:
         with open(corpus) as corpus_file:
             corpus_lines = corpus_file.read()
         self.model = markovify.Text(corpus_lines)
-        
+
     def tweet(self):
         message = self.model.make_short_sentence(140)
         try:
@@ -50,9 +50,6 @@ class TweetBot:
         for i in range (limit):
             self.tweet()
             sleep(delay)
-    
-    def reply(self, reply_within):
-        pass
 
 
 def main():
@@ -60,14 +57,10 @@ def main():
     parser.add_argument("corpus", help="filename of the text corpus used as input for the Markov chain")
     parser.add_argument("-d", "--delay", type=int, default=7200, help="delay in seconds between tweets (default: 7200)")
     parser.add_argument("-l", "--limit", type=int, default=0, help="number of tweets to send before exit (default: no limit)")
-    parser.add_argument("-r", "--reply", type=int, default=0, help="instead of writing new tweets, reply to @mentions from within the past X seconds")
     args = parser.parse_args()
     
     bot = TweetBot(args.corpus)
-    if args.reply:
-        bot.reply(args.reply)
+    if args.limit:
+        bot.automate_with_limit(args.delay, args.limit)
     else:
-        if args.limit:
-            bot.automate_with_limit(args.delay, args.limit)
-        else:
-            bot.automate(args.delay)
+        bot.automate(args.delay)
